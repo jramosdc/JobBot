6de147ee7b302170fc0acba60f1bd0fa9418a52a
Jairo Mejia <mail.jairo@gmail.com>
2016-02-26 12:09:04 -0500

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,18 +1,5 @@
 # Modules ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-import argparse
-
 from weekly_employment_report import app
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-
-# Main ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-if __name__ == '__main__':
-    # Obtener los argumentos suministrados por línea de comandos
-    args = build_command_line_interface_parser().parse_args()
-
-    # Asociar a la aplicación el manejador de registros de eventos
-    if 'LOGGER_HANDLER' in app.config:
-        app.logger.addHandler(app.config['LOGGER_HANDLER'])
-
-    # Levantar el servidor de desarrollo
-    app.run(host=args.host, port=args.port)
+if __name__ == "__main__":	
+   app.run()
