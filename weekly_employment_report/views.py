f9c0d8ef7f431ec76ccc30729df90af5b5b0290a
Jairo Mejia <mail.jairo@gmail.com>
2016-03-14 11:23:13 -0400

    Update views.py

--- weekly_employment_report/views.py
+++ weekly_employment_report/views.py
@@ -10,6 +10,8 @@ import nltk.data
 from flask import render_template
 from flask import request
 from oauth2client.client import SignedJwtAssertionCredentials
+import python_usdol
+import json,datetime,re
 
 from . import app
 from . import helpers
@@ -31,12 +33,40 @@ def index():
         ['https://spreadsheets.google.com/feeds']
     ))
 
+   #DOLAPI
+    conn = python_usdol.Connection(token='bf7867cd-4297-40ec-8fcb-f03597aa6dfe', secret='appl')  
+
+    data = conn.fetch_data("Statistics/OUI_InitialClaims","unemploymentInsuranceInitialClaims", skip=2508)
+
+    jsondata= json.dumps(data)
+
+    jsondata1= json.loads(jsondata)
+
+    #cleandataDOLAPI
+    regex= r'[a-zA-Z()/]+'
+    pat= re.compile(regex)
+    ultimo=jsondata1[-1]['seasonallyAdjustedInitialClaims']
+    results = []
+    fecha=[]
+    for i in reversed(jsondata1):
+        time=i['week']
+        timeregex= float(pat.sub('',time))
+        timeread= datetime.datetime.fromtimestamp(timeregex/1000.0).strftime("%B %d, %Y")
+        figure=i['seasonallyAdjustedInitialClaims']
+        results.append(int(figure))
+        fecha.append(timeread) 
+    if ultimo>results[1]:
+        linea0= ' Higher than the previous week.'
+    else:
+        match=next(obj for obj in results if obj<ultimo)
+        position=results.index(match)
+        linea0=u' '.join([' The lowest since the week of']+[fecha[position]]+['.'])
     # Procesar
     tokenizer = nltk.data.load('nltk:tokenizers/punkt/english.pickle')
     lineas = tokenizer.tokenize(text)
     palabras = nltk.word_tokenize(text)
     cifra1 = u' '.join(palabras[54:58])
-    cifra2 = u' '.join(palabras[49:53]+[', according to data of the Department of Labor'])
+    cifra2 = u' '.join(palabras[49:53]+[', according to data of the Department of Labor'])+linea0
     linea1 = u' '.join(palabras[36:62]).replace(u' ,', u',')
     linea2 = u' '.join(lineas[1:2])
     linea3 = u' '.join(lineas[4:5])
